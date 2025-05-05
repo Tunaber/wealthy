924c2305674e84d6473fc068c1c219234d065ac0
Копылов Никита Вячеславович <Ative66@gmail.com>
2025-05-05 16:50:33 +0700

    Реализована страница с регистрацией и авторизацией

--- Wealthy/src/App.jsx
+++ Wealthy/src/App.jsx
@@ -6,11 +6,13 @@ import MortgageCalculator from './Components/MortgageCalculator/MortgageCalculat
 import CurrencyConverter from './Components/CurrencyConverter/CurrencyConverter';
 import PodcastsPage from './Components/Podcasts/PodcastsPage';
 import NewsPage from './Components/News/NewsPage';
+import RegLogPage from './Components/Auth/RegLogPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/auth" element={<RegLogPage />} />
       <Route path="/calendar" element={<FinanceCalendar />} />
       <Route path="/mortgage" element={<MortgageCalculator />} />
       <Route path="/converter" element={<CurrencyConverter />} />
